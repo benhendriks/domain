d635e282dfb730254246bddc72d8da24ca706fd3
Benjamin Steidl <localartpool@gmail.com>
2020-08-19 08:54:44 +0200

    Updates

---
+++ precache-manifest.45bf05f63aec2fbf8c1ff33c0be25981.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef70b070b40eb2a65a9fae7518a749cd",
+    "url": "/domain/index.html"
+  },
+  {
+    "revision": "23f5606b2a1cc5f9d730",
+    "url": "/domain/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c15476fcfc30e59c038e",
+    "url": "/domain/static/js/2.f976d039.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/domain/static/js/2.f976d039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23f5606b2a1cc5f9d730",
+    "url": "/domain/static/js/main.241cc99e.chunk.js"
+  },
+  {
+    "revision": "7b871dd9598489304615",
+    "url": "/domain/static/js/runtime-main.baacd314.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/domain/static/media/logo.5d5d9eef.svg"
+  }
+]);
