137ef48b5e9760bbccad8ce7abe3c22211837052
Benjamin Steidl <localartpool@gmail.com>
2020-08-25 18:31:59 +0200

    Changed milk text and images

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,33 +8,42 @@ const About=() => (
         <h2>About me,</h2>
           <br />
             <p>
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-              sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-              rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-              ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
-              elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-              aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
-              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
-              est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-              sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-              dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-              et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-              takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum
-              iriure dolor in hendrerit in vulputate velit esse molestie consequat,
-              vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan
-              et iusto odio dignissim qui blandit praesent luptatum zzril delenit
-              augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit
-              amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
-              tincidunt ut laoreet dolore magna aliquam erat volutpat.
-              Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper
-              suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem
-              vel eum iriure dolor in hendrerit in vulputate velit esse molestie
-              consequat, vel illum dolore eu feugiat nulla facilisis at vero eros
-              et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril
-              delenit augue duis dolore te feugait nulla facilisi.
-              Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet
-              doming id quod mazim placerat facer
+              Welcome to my website, where I’ll be sharing my (ongoing) experience
+              of going zero.
+              <br/>
+              <br/>
+              <h3 className="about-highlight">zero plastic, zero waste, zero bad vibes, …</h3>
+              <br/>
+              Going zero is not one single, big overnight change, but consists
+              of many little baby steps concerning changes ranging from food to
+              clothing, packaging, “beauty products” etc.
+              <br/>
+              <br/>
+              How did I get started, you might wonder?
+              Well, almost 1.5 years ago, I made the decision to eat fewer dairy
+              products. Then I decided to try to avoid buying things that come
+              in plastic packaging. A few months later, another decision followed:
+              making my own deodorant... So slowly but steadily I changed my habits
+              and products I use. There are always new things to try and to change.
+              So, I'm curious to see where this journey will take me next!
+              The changes I’ve made so far don’t feel like drastic changes, but
+              every little change adds to the bigger picture of living a more
+              sustainable life. Basically, it all started with one little decision
+              and then one thing led to another.
+              <br/>
+              <br/>
+              And I’m happy to say that throughout my journey I stayed 100% fluffy
+              - happy, comfortable, and cosy!
+              <br/>
+              <br/>
+              I hope of encouraging you to take
+              a step towards a cleaner future, I created this website. Nobody is
+              perfect – me included – and you shouldn’t feel pressured to change
+              all your habits and routines overnight. But every little effort helps!
+              Please, join me and everyone who is already on this journey and
+              hopefully get inspired to start your own!
+              Stay fluffy,
+              Regula
             </p>
       </div>
         <img src="https://res.cloudinary.com/daf8fgekl/image/upload/v1597929812/gozerostayfluffy_iac0jd.png" alt="fluffy" />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -34,42 +34,54 @@ const Home=() => (
           <img src="https://res.cloudinary.com/daf8fgekl/image/upload/v1597929812/gozerostayfluffy_iac0jd.png" alt="fluffy" />
         </div>
         <div className="tweet">
-          <img src="https://res.cloudinary.com/daf8fgekl/image/upload/v1597929812/almondmilk_dnm7yy.png" alt="almondmilk"/>
+          <img src="https://res.cloudinary.com/daf8fgekl/image/upload/v1598372771/almond-milk_g2jcxb.png" alt="almondmilk"/>
             <div className="tweet-text">
                 <h2>Milk,</h2>
                   <br />
                     <p>
-                      My first decision at the beginning of 2019 was to reduce dairy
-                      products. Having come out the other end of the puberty-tube
-                      around 10 years ago, I felt like I should have clearer skin
-                      and not those light acne pimples on my face that I first noticed
-                      a few years back...
-                      So, I tried to find out what might help. I read various tips in
-                      different forums, like drinking mint tea would help, which I
-                      tried with little to no effect. Further into my research, I
-                      read that there could be a correlation between the amount of
-                      dairy intake and acne due to iodine and/or hormones in the milk
-                      which might exacerbate acne prone skin.
+                      The beginning of my journey: less dairy - better skin
+                      My first decision at the beginning of 2019 was to reduce
+                      dairy products. Having come out the other end of the
+                      puberty-tube around 10 years ago, I felt like I should have
+                      clearer skin and not those light acne pimples on my face
+                      that I first noticed a few years back...
+                      So, I tried to find out what might help. I grazed the
+                      internet and read various tips in different forums. For
+                      example, I started drinking mint tea, which had little to
+                      no effect. Further into my research, I read that there
+                      could be a correlation between the amount of dairy intake
+                      and acne due to iodine and/or hormones in the milk which
+                      might exacerbate acne prone skin.
                       See for example:
                       <br/>
-                      <br />
-                        <a href="https://www.jaad.org/article/S0190-9622(05)02097-9/pdf">
+                      <br/>
+                        <a href="https://www.jaad.org/article/S0190-9622(05)02097-9/pdf" target="_blank">
                           Jaad
                         </a>
                       <br />
-                        <a href="https://jandonline.org/article/S0002-8223(09)01867-7/abstract">
+                        <a href="https://jandonline.org/article/S0002-8223(09)01867-7/abstract" target="_blank">
                           Jandonline
                         </a>
                       <br />
                       <br />
-                      After having tried clearing my skin by upping my mint tea intake,
-                      I decided to drop my milk drinking habit at the end of January
-                      2019. No more milk - still the occasional cheese or milk in
-                      lasagne but nor more porridge or muesli made with milk - for
-                      a couple of months to see if there would be any visible
-                      improvements. Eight months later I still follow this dietary
-                      habit. So guess what!?! In my case, renouncing regular milk
-                      intake really helped my skin looking more healthy. :D
+                      After having tried clearing my skin by upping my mint tea
+                      intake, I decided to drop my milk drinking habit at the
+                      end of January 2019. Up to then I used to drink at least a
+                      big glass of milk every day. So, this was a big decision
+                      for me. No more milk - still the occasional cheese or milk
+                      in lasagne but nor more porridge or muesli made with
+                      ordinary milk - for a couple of months to see if there would
+                      be any visible improvements. Eight months later I was still
+                      following this dietary habit. And guess what!?! In my case,
+                      renouncing regular milk intake really helped my skin looking
+                      healthier. :D Of course, I had to find an alternative to
+                      animal milk. Which is why I turned to plant-based milk. For
+                      a while, I was content buying ready made almond or soy milk.
+                      However, I didn’t like the waste that came with it. Which
+                      is why I decided to make my own &nbsp;
+                      <Link to="/recipies" className="almon-milk-link">
+                        almond milk...
+                      </Link>
                     </p>
               </div>
             </div>
@@ -91,27 +103,7 @@ const Home=() => (
                     dolor sit amet.
                   </p>
             </div>
-          <img src="https://res.cloudinary.com/daf8fgekl/image/upload/v1597930526/Deo_wp64yk.jpg" alt="Deo"/>
-        </div>
-        <div className="tweet">
-          <img src="https://res.cloudinary.com/daf8fgekl/image/upload/v1597930835/Reiniger_edhqra.jpg" alt="Cleaner"/>
-            <div className="tweet-text">
-              <h2>Cleaner,</h2>
-                <br />
-                  <p>
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-                    sed diam nonumy eirmod tempor invidunt ut labore et dolore
-                    magna aliquyam erat, sed diam voluptua. At vero eos et
-                    accusam et justo duo dolores et ea rebum. Stet clita kasd
-                    gubergren, no sea takimata sanctus est Lorem ipsum dolor
-                    sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
-                    elitr, sed diam nonumy eirmod tempor invidunt ut labore
-                    et dolore magna aliquyam erat, sed diam voluptua. At vero
-                    eos et accusam et justo duo dolores et ea rebum. Stet clita
-                    kasd gubergren, no sea takimata sanctus est Lorem ipsum
-                    dolor sit amet.
-                  </p>
-            </div>
+          <img src="https://res.cloudinary.com/daf8fgekl/image/upload/v1598372912/selfmade-deo_xdhhop.png" alt="Deo"/>
         </div>
       </div>
     </div>
